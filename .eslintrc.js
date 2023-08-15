dce67baef11f5d14e7b49546b4400d0d129ee5c9
shanwker1223 <gabriellucasdev@gmail.com>
2023-08-15 21:56:09 +0200

    chore(update): :package: Update NX (#31)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,43 @@
 module.exports = {
-  "root": true,
-  "ignorePatterns": ["**/*"],
-  "plugins": ["@nrwl/nx", "autofix", "import"],
-  "overrides": [
+  root: true,
+  ignorePatterns: ['**/*'],
+  plugins: ['@nx', 'autofix', 'import'],
+  overrides: [
     {
-      "files": ["*.ts", "*.tsx", "*.js", "*.jsx"],
-      "rules": {
-        "@nrwl/nx/enforce-module-boundaries": [
-          "error",
+      files: ['*.ts', '*.tsx', '*.js', '*.jsx'],
+      rules: {
+        '@nx/enforce-module-boundaries': [
+          'error',
           {
-            "enforceBuildableLibDependency": true,
-            "allow": [],
-            "depConstraints": [
+            enforceBuildableLibDependency: true,
+            allow: [],
+            depConstraints: [
               {
-                "sourceTag": "*",
-                "onlyDependOnLibsWithTags": ["*"]
-              }
-            ]
-          }
-        ]
-      }
-    },
-    {
-      "files": ["*.ts", "*.tsx"],
-      "extends": ["plugin:@nrwl/nx/typescript"],
-      "rules": {}
-    },
-    {
-      "files": ["*.js", "*.jsx"],
-      "extends": ["plugin:@nrwl/nx/javascript"],
-      "rules": {}
-    },
-    {
-      "files": ["*.spec.ts", "*.spec.tsx", "*.spec.js", "*.spec.jsx"],
-      ...require('eslint-plugin-jest').configs.recommended,
-      "env": {
-        "jest": true,
-        'jest/globals': true
+                sourceTag: '*',
+                onlyDependOnLibsWithTags: ['*'],
+              },
+            ],
+          },
+        ],
       },
-    }
-  ]
-}
+    },
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: ['plugin:@nx/typescript'],
+      rules: {},
+    },
+    {
+      files: ['*.js', '*.jsx'],
+      extends: ['plugin:@nx/javascript'],
+      rules: {},
+    },
+    {
+      files: ['*.spec.ts', '*.spec.tsx', '*.spec.js', '*.spec.jsx'],
+      ...require('eslint-plugin-jest').configs.recommended,
+      env: {
+        jest: true,
+        'jest/globals': true,
+      },
+    },
+  ],
+};
--- jest.preset.js
+++ jest.preset.js
@@ -1,20 +1,19 @@
-const nxPreset = require('@nrwl/jest/preset').default;
-
+const nxPreset = require('@nx/jest/preset').default;
 
 module.exports = {
-    ...nxPreset,
-    collectCoverage: true,
-    collectCoverageFrom: [
-      '**/*.ts',
-      '!**/jest.config.ts',
-      '!src/index.ts',
-      '!src/lib/**/index.ts',
-    ],
-    coverageReporters: ['html'],
-    coverageThreshold: {
-      global: {
-        lines: 100,
-        branches: 100
-      },
+  ...nxPreset,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.ts',
+    '!**/jest.config.ts',
+    '!src/index.ts',
+    '!src/lib/**/index.ts',
+  ],
+  coverageReporters: ['html'],
+  coverageThreshold: {
+    global: {
+      lines: 100,
+      branches: 100,
     },
+  },
 };
